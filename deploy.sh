ea929962a9a8e0233014714c546e87f88013c884
Anthony Salemo <ajssalemo@gmail.com>
2022-03-21 17:17:48 -0400

    testing once more

--- deploy.sh
+++ deploy.sh
@@ -80,7 +80,7 @@ fi
 if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
   echo "Running custom bash build script.."
-  echo "Running yarn install.."
+  echo "Running yarn install and running yarn config delete proxy.."
   yarn config delete proxy
   yarn install 
   echo "Running yarn run build.." 
